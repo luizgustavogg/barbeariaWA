68962f32740fc90c4e4d314001b5c8f8f0ad090c
luizgustavogg <dmwkakskamkass@gmail.com>
2023-02-09 08:59:42 -0300

    popup + faltando back-end

---
+++ assets/js/show-popup.js
@@ -0,0 +1,32 @@
+const popupTrigger = document.querySelector('.popup-trigger');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close');
+
+popupTrigger.addEventListener('click', (e) => {
+  popup.classList.add('show');
+  document.body.style.cssText = `overflow: hidden;`;
+
+});
+
+popupClose.addEventListener('click', (e) => {
+  popup.classList.remove('show');
+  document.body.style.cssText = '';
+});
+
+// close on click on overlay
+
+popup.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    popup.classList.remove('show');
+    document.body.style.cssText = '';
+  }
+});
+
+// close on press of escape button
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && popup.classList.contains('show')) {
+      popup.classList.remove('show');
+      document.body.style.cssText = '';
+    }
+  });
